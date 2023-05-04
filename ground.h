b0601a393ab0d2a8ae33472286c2d3d00723558f
Alice Anselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-05-04 11:24:39 +0200

    ground vertices

---
+++ ground.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<glm/glm.hpp>
+
+float groundVertices[] = {
+    // positions          // colors
+    -5.0f, -0.5f, -5.0f,  0.2f, 0.8f, 0.2f,
+     5.0f, -0.5f, -5.0f,  0.2f, 0.8f, 0.2f,
+     5.0f, -0.5f,  5.0f,  0.2f, 0.8f, 0.2f,
+     5.0f, -0.5f,  5.0f,  0.2f, 0.8f, 0.2f,
+    -5.0f, -0.5f,  5.0f,  0.2f, 0.8f, 0.2f,
+    -5.0f, -0.5f, -5.0f,  0.2f, 0.8f, 0.2f
+};
