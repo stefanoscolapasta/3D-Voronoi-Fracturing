1938f2cf30bc1ddf590132c1cda4fc1cad4d5f2d
Alice Anselmi <87475490+AliceAnselmi@users.noreply.github.com>
2023-05-04 11:20:19 +0200

    cube falling down to the ground with bullet integration

--- Main.cpp
+++ Main.cpp
@@ -2,14 +2,15 @@
 #include "stb_image.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
+#include <bullet/btBulletCollisionCommon.h>
+#include <bullet/btBulletDynamicsCommon.h>
+#include"cube.h"
 #include "shader.h"
 #include "camera.h"
+#include"ground.h"
 
 #include <iostream>
 
@@ -31,20 +32,31 @@ bool firstMouse = true;
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 
+// physics variables
+btBroadphaseInterface* broadphase;
+btDefaultCollisionConfiguration* collisionConfiguration;
+btCollisionDispatcher* dispatcher;
+btSequentialImpulseConstraintSolver* solver;
+btDiscreteDynamicsWorld* dynamicsWorld;
+
+
 int main()
 {
     // glfw: initialize and configure
-    // ------------------------------
+// ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
+
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
+  
 
-    // glfw window creation
+
     // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -79,63 +91,7 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
-        //Vertex coords       //Colors
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.3f, 0.3f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-         0.5f,  0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-         0.5f,  0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.4f, 0.2f,
-
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.5f, 0.1f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.5f, 0.1f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.5f, 0.1f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.5f, 0.1f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.5f, 0.1f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.5f, 0.1f,
-
-         0.5f,  0.5f,  0.5f,  0.0f, 0.6f, 0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f, 0.6f, 0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 0.6f, 0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 0.6f, 0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.6f, 0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f, 0.6f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.7f, 0.4f,
-         0.5f, -0.5f, -0.5f,  0.0f, 0.7f, 0.4f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.7f, 0.4f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.7f, 0.4f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.7f, 0.4f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.7f, 0.4f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.8f, 0.3f,
-         0.5f,  0.5f, -0.5f,  0.0f, 0.8f, 0.3f,
-         0.5f,  0.5f,  0.5f,  0.0f, 0.8f, 0.3f,
-         0.5f,  0.5f,  0.5f,  0.0f, 0.8f, 0.3f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.8f, 0.3f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 0.8f, 0.3f,
-    };
-    // world space positions of our cubes
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
+    
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -144,6 +100,23 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // set up vertex data for ground plane
+
+    unsigned int groundVBO, groundVAO;
+    glGenVertexArrays(1, &groundVAO);
+    glGenBuffers(1, &groundVBO);
+
+    glBindVertexArray(groundVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -151,12 +124,50 @@ int main()
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+
     
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
     ourShader.use();
     // render loop
     // -----------
+
+        //initialize physics world
+    // -----------------------------
+    // Broadphase, Collision Configuration, Dispatcher, Solver, and World
+    broadphase = new btDbvtBroadphase();
+    collisionConfiguration = new btDefaultCollisionConfiguration();
+    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    solver = new btSequentialImpulseConstraintSolver;
+    dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    dynamicsWorld->setGravity(btVector3(0, -9.81, 0)); // set gravity to -9.81 in the y direction
+
+    // create a rigid body for the cube
+    btTransform startTransform;
+    startTransform.setIdentity();
+    startTransform.setOrigin(cubePositions[0]);
+    btScalar mass(1.0f);
+    btVector3 localInertia(0, 0, 0);
+    btCollisionShape* shape = new btBoxShape(btVector3(0.5f, 0.5f, 0.5f));
+    btDefaultMotionState* cubeMotionState = new btDefaultMotionState(startTransform);
+    shape->calculateLocalInertia(mass, localInertia);
+    btRigidBody::btRigidBodyConstructionInfo rigidBodyCI(mass, cubeMotionState, shape, localInertia);
+    btRigidBody* cubeRigidBody = new btRigidBody(rigidBodyCI);
+    cubeRigidBody->setWorldTransform(startTransform);
+
+    // create a rigid body for the ground plane
+    btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0),0);
+    btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+    btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+
+    // add the rigid body to the physics world
+    dynamicsWorld->addRigidBody(cubeRigidBody);
+    // add the ground rigid body to the physics world
+    dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+
     int i = 0;
 
     while (!glfwWindowShouldClose(window))
@@ -190,9 +201,17 @@ int main()
         // render boxes
         glBindVertexArray(VAO);
 
-        // calculate the model matrix for each object and pass it to shader before drawing
+        // step the physics simulation
+        dynamicsWorld->stepSimulation(deltaTime, 10);
+
+        // get the transform of the rigid body representing the cube
+        btTransform trans;
+        cubeRigidBody->getMotionState()->getWorldTransform(trans);
+
+        // update the position of the cube
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, cubePositions[0]);
+        model = glm::translate(model, glm::vec3(trans.getOrigin().getX(), trans.getOrigin().getY(), trans.getOrigin().getZ()));
+        ourShader.setMat4("model", model);
 
         float angle = 0; // *i
 
@@ -236,6 +255,7 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
